27b2a0a0f6efc35eb0c56efaf458548eee395228
Lakesh Sahu <lakeshsahu22@gmail.com>
2024-08-19 12:58:46 +0530

    ME_QA_XFLIPKART_SEARCH_MODULE_L1

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,38 +1,95 @@
 package demo;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.logging.LogType;
 import org.openqa.selenium.logging.LoggingPreferences;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
 
 // import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
     ChromeDriver driver;
+    WebDriverWait wait;
+    Wrappers wp;
 
     /*
-     * TODO: Write your tests here with testng @Test annotation. 
-     * Follow `testCase01` `testCase02`... format or what is provided in instructions
+     * TODO: Write your tests here with testng @Test annotation.
+     * Follow `testCase01` `testCase02`... format or what is provided in
+     * instructions
      */
+    @Test
+    public void testCase01() throws InterruptedException {
+        System.out.println("Starting Test case: testCase01");
 
-     
+        wp.openUrl();
+
+        wp.searchProduct("Washing Machine");
+
+        WebElement popularityBtn = wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class='zg-M3Z' and text()='Popularity']")));
+        popularityBtn.click();
+
+        wait.until(ExpectedConditions
+                .visibilityOfElementLocated(By.xpath("//div[@class='zg-M3Z _0H7xSG' and text()='Popularity']")));
+
+        List<WebElement> ratings = driver
+                .findElements(By.xpath("//div[@class='DOjaWF gdgoEp']//div[@class='XQDdHH' and text()<=4]"));
+
+        System.out.println("Number of items whose rating is equal to or less than 4 = " + ratings.size());
+
+        System.out.println("end Test case: testCase01");
+    }
+
+    @Test
+    public void testCase02() throws InterruptedException {
+        System.out.println("Starting Test case: testCase02");
+        wp.searchProduct("iPhone");
+        List<WebElement> off = driver.findElements(By.xpath("//div[@class='DOjaWF gdgoEp']//span[contains(text(),'% off')]"));
+        for (WebElement element : off) {
+            if (wp.offValue(element) > 17) {
+                WebElement title = driver.findElement(RelativeLocator.with(By.className("KzDlHZ")).toLeftOf(element));
+                System.out.println(title.getText());
+            }
+        }
+        System.out.println("end Test case: testCase02");
+    }
+
+    @Test
+    public void testCase03() throws InterruptedException {
+        System.out.println("Starting Test case: testCase03");
+
+        wp.searchProduct("Coffee Mug");
+        
+        wp.ratingSelection(4);
+
+        wp.reviewCounter();
+
+        System.out.println("end Test case: testCase03");
+    }
+
     /*
-     * Do not change the provided methods unless necessary, they will help in automation and assessment
+     * Do not change the provided methods unless necessary, they will help in
+     * automation and assessment
      */
     @BeforeTest
-    public void startBrowser()
-    {
+    public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
 
         // NOT NEEDED FOR SELENIUM MANAGER
@@ -46,17 +103,20 @@ public class TestCases {
         options.setCapability("goog:loggingPrefs", logs);
         options.addArguments("--remote-allow-origins=*");
 
-        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log"); 
+        System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
         driver = new ChromeDriver(options);
 
         driver.manage().window().maximize();
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(30));
+
+        wp = new Wrappers(driver, wait);
     }
 
     @AfterTest
-    public void endTest()
-    {
-        driver.close();
+    public void endTest() {
+        // driver.close();
         driver.quit();
 
     }
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -11,6 +11,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
@@ -19,4 +20,106 @@ public class Wrappers {
     /*
      * Write your selenium wrappers here
      */
+    ChromeDriver driver;
+    WebDriverWait wait;
+
+    public Wrappers(ChromeDriver driver, WebDriverWait wait) {
+        this.driver = driver;
+        this.wait = wait;
+    }
+
+    public void openUrl() {
+        driver.get("https://www.flipkart.com");
+    }
+
+    public void click(WebElement element) {
+        element.click();
+    }
+
+    public void sendKeys(WebElement element, String text) {
+        element.clear();
+        element.sendKeys(text);
+    }
+
+    public void searchProduct(String productName) throws InterruptedException {
+        Thread.sleep(5000);
+        WebElement searchBar = driver.findElement(By.xpath("//input[@title]"));
+        searchBar.click();
+        searchBar.clear();
+        searchBar.sendKeys(productName);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title and @value = '" + productName + "']")));
+        searchBar.sendKeys(Keys.ENTER);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_1G0WLw']/span[contains(text(), 'Page')]")));
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            // TO DO: handle exception
+            System.out.println("No product found");
+        }
+    }
+
+    public int offValue(WebElement element) {
+        String nums = "0123456789";
+        String s = element.getText();
+        int i = 0;
+        for (i = 0; i < s.length(); i++) {
+            if (!nums.contains(Character.toString(s.charAt(i)))) {
+                break;
+            }
+        }
+        return Integer.parseInt(s.substring(0, i));
+    }
+
+    public void ratingSelection(int n) throws InterruptedException {
+        WebElement checkBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(), '" + n + "') and contains(text(),' & above')]/preceding-sibling::div")));
+        System.out.println("4* found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        checkBox.click();
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_6tw8ju']")));
+    }
+
+    public void reviewCounter() {
+        ArrayList<Integer> ratingNums = new ArrayList<>();
+        List<WebElement> reviewsCo = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+        for (WebElement webElement : reviewsCo) {
+            String ratingWithBrak = webElement.getText().replace("(", "");
+            String ratingWOBrack = ratingWithBrak.replace(")", "");
+            String ratingWOComma = ratingWOBrack.replace(",", "");
+            int rating = Integer.parseInt(ratingWOComma);
+            ratingNums.add(rating);
+        }
+        Collections.sort(ratingNums, Collections.reverseOrder());
+        for(int i = 0; i < 5; i++) {
+            int singleRating = ratingNums.get(i);
+            String singleRatingString = String.valueOf(singleRating);
+            String withComma;
+            if(singleRating > 999) {
+                if (singleRatingString.length() == 4) {
+                    withComma = singleRatingString.substring(0, 1) + "," + singleRatingString.substring(1);
+                }
+                else if (singleRatingString.length() == 5) {
+                    withComma = singleRatingString.substring(0, 2) + "," + singleRatingString.substring(2);
+                }
+                else {
+                    withComma = "000";
+                }
+            }
+            else {
+                withComma = singleRatingString;
+            }
+
+            int sameCount = 1;
+            for(int j = 0; j < i; j++) {
+                if (singleRatingString.equals(String.valueOf(ratingNums.get(j)))) {
+                    sameCount++;
+                }
+            }
+            WebElement forTitle = driver.findElement(By.xpath("(//span[text()='(" + withComma + ")']/parent::div/preceding-sibling::a[1])[" + sameCount + "]"));
+            String title = forTitle.getAttribute("title");
+            WebElement forSRC = driver.findElement(By.xpath("(//span[text()='(" + withComma + ")']/parent::div/preceding-sibling::a[2]//img)[" + sameCount + "]"));
+            String src = forSRC.getAttribute("src");
+    
+            System.out.println("Title : " + title);
+            System.out.println("SRC : " + src);
+        }
+    }
 }
