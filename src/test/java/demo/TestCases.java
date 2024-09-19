851bf3e20c4c572b41f9dcc8905b9c3f1c2cee28
Lakesh Sahu <lakeshsahu22@gmail.com>
2024-09-19 23:26:14 +0530

    Revised

--- src/test/java/demo/TestCases.java
+++ src/test/java/demo/TestCases.java
@@ -1,8 +1,6 @@
 package demo;
 
 import org.openqa.selenium.By;
-// import org.openqa.selenium.Keys;
-// import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
@@ -15,13 +13,9 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.time.Duration;
-// import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
-
-// import io.github.bonigarcia.wdm.WebDriverManager;
 import demo.wrappers.Wrappers;
 
 public class TestCases {
@@ -30,64 +24,108 @@ public class TestCases {
     Wrappers wp;
 
     /*
-     * TO DO: Write your tests here with testng @Test annotation.
-     * Follow `testCase01` `testCase02`... format or what is provided in
-     * instructions
+     * Go to www.flipkart.com. Search "Washing Machine". Sort by popularity and
+     * print the count of items with rating less than or equal to 4 stars.
      */
     @Test
     public void testCase01() throws InterruptedException {
+        try {
         System.out.println("Starting Test case: testCase01");
 
+        //Opening flipkart url
         wp.openUrl();
 
+        //Searching for product 
         wp.searchProduct("Washing Machine");
 
+        //Waiting and Locating popularity button
         WebElement popularityBtn = wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.xpath("//div[@class='zg-M3Z' and text()='Popularity']")));
+
+        //Performing click on popularity button
         popularityBtn.click();
 
+        //Waiting for appearance of popularity button after clicking
         wait.until(ExpectedConditions
                 .visibilityOfElementLocated(By.xpath("//div[@class='zg-M3Z _0H7xSG' and text()='Popularity']")));
 
+        //Locating rating WebElements having rating less than 4
         List<WebElement> ratings = driver
                 .findElements(By.xpath("//div[@class='DOjaWF gdgoEp']//div[@class='XQDdHH' and text()<=4]"));
 
         System.out.println("Number of items whose rating is equal to or less than 4 = " + ratings.size());
 
+        System.out.println("testCase01 Passed");
+        } catch (Exception e) {
+            System.out.println("testCase01 Failed");
+            e.printStackTrace();
+        }
         System.out.println("end Test case: testCase01");
     }
 
+    /*
+     * Search "iPhone", print the Titles and discount % of items with more than 17%
+     * discount
+     */
     @Test
     public void testCase02() throws InterruptedException {
+        try {
         System.out.println("Starting Test case: testCase02");
+
+        //Searching for product
         wp.searchProduct("iPhone");
-        List<WebElement> off = driver.findElements(By.xpath("//div[@class='DOjaWF gdgoEp']//span[contains(text(),'% off')]"));
+
+        //Storing List of WebElements which contains "% off" text
+        List<WebElement> off = driver
+                .findElements(By.xpath("//div[@class='DOjaWF gdgoEp']//span[contains(text(),'% off')]"));
+        
+        //Iterating through each WebElement stored above
         for (WebElement element : off) {
+
+            //Getting the Integer off and comparing with > 17
             if (wp.offValue(element) > 17) {
+
+                //Getting the title of product whose Integer off is greater than 17 
                 WebElement title = driver.findElement(RelativeLocator.with(By.className("KzDlHZ")).toLeftOf(element));
+
+                //Printing the title
                 System.out.println(title.getText());
             }
+        }
+        System.out.println("testCase02 Passed");
+        } catch (Exception e) {
+            System.out.println("testCase02 Failed");
+            e.printStackTrace();
         }
         System.out.println("end Test case: testCase02");
     }
 
+    /*
+     * Search "Coffee Mug", select 4 stars and above, and print the Title and image
+     * URL of the 5 items with highest number of reviews
+     */
     @Test
     public void testCase03() throws InterruptedException {
+        try {
         System.out.println("Starting Test case: testCase03");
 
+        //Searching for product
         wp.searchProduct("Coffee Mug");
-        
+
+        //Selecting rating and above
         wp.ratingSelection(4);
 
+        //Printing the title and src value of top 5 most review count product
         wp.reviewCounter();
 
+        System.out.println("testCase03 Passed");
+        } catch (Exception e) {
+            System.out.println("testCase03 Failed");
+            e.printStackTrace();
+        }
         System.out.println("end Test case: testCase03");
     }
 
-    /*
-     * Do not change the provided methods unless necessary, they will help in
-     * automation and assessment
-     */
     @BeforeTest
     public void startBrowser() {
         System.setProperty("java.util.logging.config.file", "logging.properties");
@@ -95,6 +133,7 @@ public class TestCases {
         // NOT NEEDED FOR SELENIUM MANAGER
         // WebDriverManager.chromedriver().timeout(30).setup();
 
+        //Creating object of ChromeOptions class
         ChromeOptions options = new ChromeOptions();
         LoggingPreferences logs = new LoggingPreferences();
 
@@ -105,17 +144,22 @@ public class TestCases {
 
         System.setProperty(ChromeDriverService.CHROME_DRIVER_LOG_PROPERTY, "build/chromedriver.log");
 
+        //Initializing a new object of ChromeDriver class
         driver = new ChromeDriver(options);
 
+        //Maximizing the browser window
         driver.manage().window().maximize();
 
+        //Creating a new instance of WebDriverWait class with driver and 30 Second of wait
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
 
+        //Instantiating a new object of Wrappers class
         wp = new Wrappers(driver, wait);
     }
 
     @AfterTest
     public void endTest() {
+        //Quiting the browser or closing all the window
         // driver.close();
         driver.quit();
 
--- src/test/java/demo/wrappers/Wrappers.java
+++ src/test/java/demo/wrappers/Wrappers.java
@@ -2,19 +2,13 @@ package demo.wrappers;
 
 import java.util.ArrayList;
 import java.util.Collections;
-// import java.util.HashMap;
 import java.util.List;
-// import java.util.Map;
-// import java.util.stream.Collectors;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
-// import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-// import java.time.Duration;
 
 public class Wrappers {
     /*
@@ -23,32 +17,47 @@ public class Wrappers {
     ChromeDriver driver;
     WebDriverWait wait;
 
+    //Constuctor of Wrappers class
     public Wrappers(ChromeDriver driver, WebDriverWait wait) {
         this.driver = driver;
         this.wait = wait;
     }
 
+    //Opening flipkart url
     public void openUrl() {
         driver.get("https://www.flipkart.com");
     }
 
+    //Clicking on an WebElement
     public void click(WebElement element) {
         element.click();
     }
 
+    //Entering text to an WebElement
     public void sendKeys(WebElement element, String text) {
         element.clear();
         element.sendKeys(text);
     }
 
+    //Searching for given product name
     public void searchProduct(String productName) throws InterruptedException {
         Thread.sleep(5000);
+
+        //Locating search bar WebElement
         WebElement searchBar = driver.findElement(By.xpath("//input[@title]"));
-        searchBar.click();
-        searchBar.clear();
-        searchBar.sendKeys(productName);
+
+        //Performing click on search bar
+        click(searchBar);
+
+        //Enetering productName to the search bar
+        sendKeys(searchBar, productName);
+        
+        //Waiting for product name to be appear on search bar
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@title and @value = '" + productName + "']")));
+        
+        //Entering Enter key
         searchBar.sendKeys(Keys.ENTER);
+
         try {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_1G0WLw']/span[contains(text(), 'Page')]")));
             Thread.sleep(5000);
@@ -58,66 +67,128 @@ public class Wrappers {
         }
     }
 
+    //Seperating Integer from "% off"
     public int offValue(WebElement element) {
         String nums = "0123456789";
         String s = element.getText();
         int i = 0;
         for (i = 0; i < s.length(); i++) {
+
+            //breaking if for loop reaches Non-Integer value
             if (!nums.contains(Character.toString(s.charAt(i)))) {
                 break;
             }
         }
+        //Retruning only the Integer part
         return Integer.parseInt(s.substring(0, i));
     }
 
+
     public void ratingSelection(int n) throws InterruptedException {
+        //Getting the checkbox for WebElement which contains "4 and above" text
         WebElement checkBox = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[contains(text(), '" + n + "') and contains(text(),' & above')]/preceding-sibling::div")));
-        System.out.println("4* found!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        
+        //Clicking on the checkbox
         checkBox.click();
+
+        //Waiting for the element "4* & above" to show on the selected filter area
         wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@class='_6tw8ju']")));
     }
 
     public void reviewCounter() {
+        //Creating Object of ArrayList to store the rating count
         ArrayList<Integer> ratingNums = new ArrayList<>();
+
+        //Getting the review WebElement
         List<WebElement> reviewsCo = driver.findElements(By.xpath("//span[@class='Wphh3N']"));
+
+        //Traversing through each review WebElement
         for (WebElement webElement : reviewsCo) {
+
+            //Replacing the "(" with ""
             String ratingWithBrak = webElement.getText().replace("(", "");
+
+            //Replacing the ")" with ""
             String ratingWOBrack = ratingWithBrak.replace(")", "");
+
+            //Replacing the "," with ""
             String ratingWOComma = ratingWOBrack.replace(",", "");
+
+            //Converting the review String into Integer
             int rating = Integer.parseInt(ratingWOComma);
+
+            //Storing review count in ratingNums as Integer
             ratingNums.add(rating);
         }
+
+        //Sorting all the review count in descending order
         Collections.sort(ratingNums, Collections.reverseOrder());
+
+        //Traversing through top 5 most review count
         for(int i = 0; i < 5; i++) {
+
+            //Getting the review count at ith place from ratingNums (descending order)
             int singleRating = ratingNums.get(i);
+
+            //Storing singleRating as String
             String singleRatingString = String.valueOf(singleRating);
             String withComma;
             if(singleRating > 999) {
+
+                //Converting four digit review count into String with comma so that we can locate the WebElement according to text
                 if (singleRatingString.length() == 4) {
                     withComma = singleRatingString.substring(0, 1) + "," + singleRatingString.substring(1);
                 }
+
+                //Converting five digit review count into String with comma so that we can locate the WebElement according to text
                 else if (singleRatingString.length() == 5) {
                     withComma = singleRatingString.substring(0, 2) + "," + singleRatingString.substring(2);
                 }
+
+                //Converting six digit review count into String with comma so that we can locate the WebElement according to text                
+                else if (singleRatingString.length() == 6) {
+                    withComma = singleRatingString.substring(0, 1) + "," + singleRatingString.substring(1, 3) + "," + singleRatingString.substring(3);
+                }
+
+                //Converting seven digit review count into String with comma so that we can locate the WebElement according to text
+                else if (singleRatingString.length() == 7) {
+                    withComma = singleRatingString.substring(0, 2) + "," + singleRatingString.substring(2, 4) + "," + singleRatingString.substring(4);
+                }
+
+                //If no review or greater than seven digit is there than give as 000
                 else {
                     withComma = "000";
                 }
             }
+
+            //Converting five digit review count into String with comma show that we can locate the WebElement according to text
             else {
                 withComma = singleRatingString;
-            }
+            } 
 
+            //Counting if two or more reviews count are same
             int sameCount = 1;
             for(int j = 0; j < i; j++) {
+
+                //if yes than increasing the count by one
                 if (singleRatingString.equals(String.valueOf(ratingNums.get(j)))) {
                     sameCount++;
                 }
             }
+
+            //Getting the title WebElement of heighest
             WebElement forTitle = driver.findElement(By.xpath("(//span[text()='(" + withComma + ")']/parent::div/preceding-sibling::a[1])[" + sameCount + "]"));
+            
+            //Getting the title as String
             String title = forTitle.getAttribute("title");
+
+            //Getting the product image as WebElement
             WebElement forSRC = driver.findElement(By.xpath("(//span[text()='(" + withComma + ")']/parent::div/preceding-sibling::a[2]//img)[" + sameCount + "]"));
+            
+            //Getting the value of src attribute of above image WebElement 
             String src = forSRC.getAttribute("src");
     
+            //Printing title and src
             System.out.println("Title : " + title);
             System.out.println("SRC : " + src);
         }
